bf8ad016e1d66d0ff7eadc42e6860a6ec8bbc2de
THE TECH GUY <65334711+jaydumisuni@users.noreply.github.com>
2026-07-24 22:39:27 +0200

    Wire optimized companion avatar modules

--- src/avatarData.ts
+++ src/avatarData.ts
@@ -1,1 +1,13 @@
-PLACEHOLDER
+import { avatars as groupA } from "./avatarsA";
+import { avatars as groupB } from "./avatarsB";
+import { avatars as groupC } from "./avatarsC";
+import { avatars as groupD } from "./avatarsD";
+
+export const avatarImages = {
+  ...groupA,
+  ...groupB,
+  ...groupC,
+  ...groupD,
+} as const;
+
+export type AvatarId = keyof typeof avatarImages;
