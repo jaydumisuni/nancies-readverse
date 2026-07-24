01651f090586286c4ba9fb68b1d8b32b86b4dc34
THE TECH GUY <65334711+jaydumisuni@users.noreply.github.com>
2026-07-24 22:58:49 +0200

    Align Mei Mei avatar key with companion profile

--- src/avatarData.ts
+++ src/avatarData.ts
@@ -3,11 +3,14 @@ import { avatars as groupB } from "./avatarsB";
 import { avatars as groupC } from "./avatarsC";
 import { avatars as groupD } from "./avatarsD";
 
+const { meimei, ...groupDRest } = groupD;
+
 export const avatarImages = {
   ...groupA,
   ...groupB,
   ...groupC,
-  ...groupD,
+  ...groupDRest,
+  meiMei: meimei,
 } as const;
 
 export type AvatarId = keyof typeof avatarImages;
